80def481f35b06ba555075fbed9b7596b816e0da
Byron Josue Ormaza Farias <bjormaza@espe.edu.ec>
2025-02-01 14:27:41 -0500

    Update footer text to reflect project details and year

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 
@@ -13,7 +12,7 @@ const Layout = () => {
       </main>
 
       <footer className='bg-gray-800 py-3 md:px-8 px-4 text-center text-white'>
-        <p>&copy; { new Date().getFullYear() } Certificate Generator. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Generador de certificados. Proyecto de Arquitectura de Software - grupo2.</p>
       </footer>
     </div>
   );
